3ab4efae6ac56ecbe8513bdeaa127175c76d1adf
GHS <20221003@edu.hanbat.ac.kr>
2025-07-30 14:57:09 +0900

    IntelliJ IDEA와 Gradle을 이용한 자바 애플리케이션 개발

--- app/src/main/java/org/example/myapp/App.java
+++ app/src/main/java/org/example/myapp/App.java
@@ -1,22 +1,23 @@
 package org.example.myapp;
 
-import org.example.util.Utils;
-import com.google.common.base.Joiner;
+import java.util.Scanner;
 
 /**
-이 클래스는 한밭대학교 특강 실습을 위한 첫 번째 클래스 입니다.<br>
-<code>App</code> 클래스!
+ * 이 클래스는 한밭대학교 특강 실습을 위한 첫 번째 클래스 입니다.<br>
+ * <code>App</code> 클래스!
  */
 public class App {
-   
-    /** 
-    이 메서드는 entry point 이다. 콘솔에 "hello, world" 메시지 출력합니다.
 
-    @param args 명령행 인수 배열
-    */
-    public static void main(String[] args) {
-        Utils.printMessage("Hello!");
-        String message = Joiner.on(", ").join("hello", "world");
-        System.out.println(message);
-    }
+  /**
+   * 이 메서드는 entry point 이다. 콘솔에 "hello, world" 메시지 출력합니다.
+   *
+   * @param args 명령행 인수 배열
+   */
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("이름? ");
+    String name = scanner.nextLine();
+    System.out.printf("안녕하세요 %s 님!\n", name);
+    scanner.close();
+  }
 }
--- app/src/main/java/org/example/util/Utils.java
+++
@@ -1,6 +0,0 @@
-package org.example.util;
-public class Utils {
-    public static void printMessage(String message) {
-        System.out.println(message);
-    }
-}
